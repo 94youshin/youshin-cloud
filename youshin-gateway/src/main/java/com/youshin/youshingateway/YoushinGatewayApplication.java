834672a04201a2793f03cf66b456460fec6e878d
yangj280 <yangjin94@163.com>
2024-12-21 23:51:44 +0800

    initial

---
+++ youshin-gateway/src/main/java/com/youshin/youshingateway/YoushinGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.youshin.youshingateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoushinGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YoushinGatewayApplication.class, args);
+    }
+
+}
---
+++ youshin-system/src/main/java/com/youshin/youshinsystem/YoushinSystemApplication.java
@@ -0,0 +1,12 @@
+package com.youshin.youshinsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoushinSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YoushinSystemApplication.class, args);
+    }
+}
